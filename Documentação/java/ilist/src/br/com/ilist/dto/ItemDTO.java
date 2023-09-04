38da667dfe7a79b4595d3a7c3b51dc47dcd08719
Victor Guerreiro <105592007+ViictorWebTech@users.noreply.github.com>
2023-09-04 16:53:51 -0300

    :books: docs: Atualizado arquivo java

--- Documentação/java/ilist/src/br/com/ilist/dto/ItemDTO.java
+++ Documentação/java/ilist/src/br/com/ilist/dto/ItemDTO.java
@@ -10,5 +10,48 @@ package br.com.ilist.dto;
  * @author Aluno
  */
 public class ItemDTO {
+    private String nome, nome_mercado;
+    private int id_item;
+    private double preco;
+
+    /**
+     * Método construtor padrão
+     */
+    public ItemDTO(){
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome_mercado() {
+        return nome_mercado;
+    }
+
+    public void setNome_mercado(String nome_mercado) {
+        this.nome_mercado = nome_mercado;
+    }
+
+    public int getId_item() {
+        return id_item;
+    }
+
+    public void setId_item(int id_item) {
+        this.id_item = id_item;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+ 
     
 }
